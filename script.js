a40869534976c9b8b2459c7faac2abd623d7b41d
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-01-06 14:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let x = window.innerWidth;
+let y = window.innerHeight;
+
+sizeInfo.innerText = `Width: ${x} and Height: ${y}`;
+window.addEventListener('resize', () => {
+	let x = window.innerWidth;
+	let y = window.innerHeight;
+	
+	sizeInfo.innerText = `Width: ${x} and Height: ${y}`;
+})
+
